24eb68939e6c69ee7633e9ace9b3d44b0035a70c
DevidMonster <mrbat905@gmail.com>
2023-04-06 00:15:33 +0700

    update layout

--- src/pages/admin/AddProduct.tsx
+++ src/pages/admin/AddProduct.tsx
@@ -73,91 +73,94 @@ function AddProduct(props: IProps) {
         },
     };
     return (
-        <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600, margin: '0 auto' }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-            validateMessages={validateMessages}
-        >
-            <Form.Item
-                label="UserName"
-                {...register('name')}
-                rules={[{ required: true }]}
-                validateStatus={errors.name ? "error" : ""}
+        <div>
+            <h1>Add Product</h1>
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600, margin: '0 auto' }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                validateMessages={validateMessages}
             >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                label="Price"
-                {...register('price')}
-                rules={[{ type: 'number', min: 0, required: true, message: `bạn phải nhập price` }]}
-                validateStatus={errors.price ? "error" : ""}
-            >
-                <InputNumber />
+                <Form.Item
+                    label="UserName"
+                    {...register('name')}
+                    rules={[{ required: true }]}
+                    validateStatus={errors.name ? "error" : ""}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Price"
+                    {...register('price')}
+                    rules={[{ type: 'number', min: 0, required: true, message: `bạn phải nhập price` }]}
+                    validateStatus={errors.price ? "error" : ""}
+                >
+                    <InputNumber />
 
-            </Form.Item>
-            <Form.Item 
-                label="Image"  
-                valuePropName="fileList"
-                {...register('image')}
-                rules={[{ required: true, message: `bạn phải chọn ảnh` }]}
-                getValueFromEvent={(e) => {
-                    if (Array.isArray(e)) {
-                      return e;
-                    }
-                    return e && e.fileList;
-                }}
-                validateStatus={errors.image ? "error" : ""}
-            >
-               
-                    <Upload
-                       name="image" beforeUpload={handleBeforeUpload} customRequest={dummyRequest} listType="picture"
-                    >
-                         <Button icon={<UploadOutlined />}>+ Upload</Button>
-                    </Upload>
+                </Form.Item>
+                <Form.Item 
+                    label="Image"  
+                    valuePropName="fileList"
+                    {...register('image')}
+                    rules={[{ required: true, message: `bạn phải chọn ảnh` }]}
+                    getValueFromEvent={(e) => {
+                        if (Array.isArray(e)) {
+                        return e;
+                        }
+                        return e && e.fileList;
+                    }}
+                    validateStatus={errors.image ? "error" : ""}
+                >
                 
-            </Form.Item>
-            <Form.Item
-                label="Description"
-                {...register('description')}
-                rules={[{ required: true, message: `bạn phải nhập description` }]}
-                validateStatus={errors.description ? "error" : ""}
-            >
-                <TextArea
-                    showCount
-                    maxLength={500}
-                    style={{ height: 120, resize: 'none' }}
-                    placeholder="description"
-                />
-            </Form.Item>
-            {/* 
-                <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item> */}
-            <Form.Item
-                name="categories"
-                label="Select[category]"
-                rules={[{ required: true, message: 'Please select catefories', type: 'array' }]}
-            >
-                <Select mode="multiple" placeholder="Please select categories">
-                    {
-                        categories?.map(cate => (
-                            <Option key={cate._id} value={cate._id}>{cate.name}</Option>
-                        ))
-                    }
-                </Select>
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+                        <Upload
+                        name="image" beforeUpload={handleBeforeUpload} customRequest={dummyRequest} listType="picture"
+                        >
+                            <Button icon={<UploadOutlined />}>+ Upload</Button>
+                        </Upload>
+                    
+                </Form.Item>
+                <Form.Item
+                    label="Description"
+                    {...register('description')}
+                    rules={[{ required: true, message: `bạn phải nhập description` }]}
+                    validateStatus={errors.description ? "error" : ""}
+                >
+                    <TextArea
+                        showCount
+                        maxLength={500}
+                        style={{ height: 120, resize: 'none' }}
+                        placeholder="description"
+                    />
+                </Form.Item>
+                {/* 
+                    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item> */}
+                <Form.Item
+                    name="categories"
+                    label="Select[category]"
+                    rules={[{ required: true, message: 'Please select catefories', type: 'array' }]}
+                >
+                    <Select mode="multiple" placeholder="Please select categories">
+                        {
+                            categories?.map(cate => (
+                                <Option key={cate._id} value={cate._id}>{cate.name}</Option>
+                            ))
+                        }
+                    </Select>
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
     );
 }
 
